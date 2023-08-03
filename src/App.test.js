8d40718663f8a1760aec7a5a258077e20985f39f
Ubuntu <andynze4@gmail.com>
2023-08-03 00:02:51 +0000

    Changes

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const titleElement = screen.getByText(/React/i);
+  const titleElement = screen.getByText('Client');
   expect(titleElement).toBeInTheDocument();
 });
